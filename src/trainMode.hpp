ed2bc76efab8788c81e8e8df0bb772266471df92
sherly <sharton1@asu.edu>
2022-03-08 12:56:18 -0500

    train and vids working again

--- src/trainMode.hpp
+++ src/trainMode.hpp
@@ -1,6 +1,10 @@
 #include <sys/stat.h>
+
 #include <iostream>
 
+#include "filter.hpp"
+#include "orProcessing.hpp"
+#include "orUtil.hpp"
 using namespace cv;
 using namespace std;
 #ifndef TRAIN_MODE_H
@@ -9,23 +13,21 @@ using namespace std;
 vector<cv::Vec3b> randomColors;
 const int maxRegions = 6;
 enum filter { none, thresh, clean, segment, features, getLabel, classify, knn};
-enum stuff { null, glasses, lwrench, mascara, noodle, plier, wire };
+enum stuff { null, glasses, knife, noodle, mascara, plier, scissors };
 
 /*
   Given a filename, and image filename, and the image features, by
   default the function will append a line of data to the CSV format
   file.  If reset_file is true, then it will open the file in 'write'
   mode and clear the existing contents.
-
   The image filename is written to the first position in the row of
   data. The values in image_data are all written to the file as
   floats.
-
   The function returns a non-zero value in case of an error.
  */
 int append_image_data_csv(char *csv_filepath, char *image_filename,
-                          char const *label_name, std::vector<float> &feature_vector,
-                          int reset_file) {
+                          const char *label_name,
+                          std::vector<float> &feature_vector, int reset_file) {
     char buffer[256];
     char mode[8];
     FILE *fp;
@@ -48,10 +50,8 @@ int append_image_data_csv(char *csv_filepath, char *image_filename,
     std::fwrite(buffer, sizeof(char), strlen(buffer), fp);
 
     // 2. label name
-    char label_formatted[256];
-    sprintf(label_formatted, ",%s",
-            label_name);  // add a comma before adding label name
-    std::fwrite(label_formatted, sizeof(char), strlen(buffer), fp);
+    sprintf(buffer, ",%s", label_name);
+    std::fwrite(buffer, sizeof(char), strlen(buffer), fp);
 
     // 3. feature vector
     // loop through feature vector
@@ -70,25 +70,24 @@ int append_image_data_csv(char *csv_filepath, char *image_filename,
     return (0);
 }
 
-
 string getNewFileName() {
     // create img name
     int fileIdx = 0;
-    string img_name = "ownClose1_";
+    string img_name = "own";
     img_name.append(to_string(fileIdx)).append(".png");
 
     // create full path
-    string path_name = "res/train/";
+    string path_name = "res/own/";
     path_name.append(img_name);
     struct stat buffer;
     bool isFileExist = (stat(path_name.c_str(), &buffer) == 0);
 
     while (isFileExist) {
         fileIdx += 1;
-        img_name = "ownClose1_";
+        img_name = "own";
         img_name.append(to_string(fileIdx)).append(".png");
 
-        path_name = "res/train/";
+        path_name = "res/own/";
         path_name.append(img_name);
         isFileExist = (stat(path_name.c_str(), &buffer) == 0);
     }
@@ -116,7 +115,7 @@ int trainMode() {
 
     printf("Expected size: %d %d\n", refS.width, refS.height);
     // 3. Create video writer object filename, format, size
-    cv::VideoWriter output("res/train/myout.avi",
+    cv::VideoWriter output("res/own/myout.avi",
                            cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 5,
                            refS);
     bool record = false;
@@ -129,8 +128,8 @@ int trainMode() {
     filter op = none;  // operation
     stuff obj = null;  // object label
 
-    // reset file
-    char csv_filepath[] = "res/train/label_train.csv";
+    char csv_filepath[] = "res/label.csv";
+    // reset file , uncomment if we want to reset
     // FILE *fp = fopen(csv_filepath, "w");
     // if (!fp) {
     //     printf("Unable to open output file %s\n", csv_filepath);
@@ -154,32 +153,32 @@ int trainMode() {
         // 2. If we are processing:
         if (op == getLabel) {
             vector<float> feature_vec;
-            cv::Point centr_of_interest;
+            cv::Point out_centroid_of_interest;
             compute_features(srcFrame, dstFrame, randomColors, maxRegions,
-                             feature_vec, centr_of_interest);
+                             feature_vec, out_centroid_of_interest);
 
-            // enum stuff { null, glasses, wire, noodle, mascara, plier };
+            // enum stuff { null, glasses, knife, noodle, mascara, plier };
 
-            if (obj != null) {  // if we are labelling, save the source frame
+            if (obj != null) {  // if we are labelling, save it as image
 
                 saved_img_name = getNewFileName();
                 char *image_filepath = (char *)saved_img_name.data();
-                string path_name = "res/sample/";
-                cv::imwrite(path_name.append(saved_img_name), srcFrame);
+                string path_name = "res/own/";
+                cv::imwrite(path_name.append(saved_img_name), dstFrame);
 
                 const char *label_name;
-                if (obj == glasses) {
-                    label_name = "glasses";
-                } else if (obj == lwrench) {
-                    label_name = "lwrench";
-                } else if (obj == mascara) {
+                if (obj == mascara) {
                     label_name = "mascara";
+                } else if (obj == glasses) {
+                    label_name = "glasses";
+                } else if (obj == knife) {
+                    label_name = "knife,";
                 } else if (obj == noodle) {
                     label_name = "noodle";
                 } else if (obj == plier) {
                     label_name = "plier";
-                } else if (obj == wire) {
-                    label_name = "wire";
+                } else if (obj == scissors) {
+                    label_name = "scissors";
                 }
 
                 append_image_data_csv(csv_filepath, image_filepath, label_name,
@@ -188,6 +187,7 @@ int trainMode() {
             }
 
             // reset object choice
+
         } else {  // display frame as is
             srcFrame.copyTo(dstFrame);
         }
@@ -211,27 +211,24 @@ int trainMode() {
         } else if (key == 'a') {
             cout << "get label" << endl;
             op = getLabel;
-        } else if (key == 'g') {
-            cout << "glasses" << endl;
-            obj = glasses;
-        } else if (key == 'l') {
-            cout << "lwrench" << endl;
-            obj = lwrench;
         } else if (key == 'm') {
             cout << "mascara" << endl;
             obj = mascara;
+        } else if (key == 'g') {
+            cout << "glasses" << endl;
+            obj = glasses;
+        } else if (key == 'k') {
+            cout << "knife" << endl;
+            obj = knife;
         } else if (key == 'n') {
             cout << "noodle" << endl;
             obj = noodle;
         } else if (key == 'p') {
             cout << "plier" << endl;
             obj = plier;
-        } else if (key == 'w') {
-            cout << "wire" << endl;
-            obj = wire;
-        } else if (key == 32) {
-            cout << "Reset color..." << endl;
-            op = none;
+        } else if (key == 's') {
+            cout << "scissors" << endl;
+            obj = scissors;
         } else if (key == -1) {
             continue;
         } else {
